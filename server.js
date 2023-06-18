5a6825ceba83882958ded77f5b7603afe2ea9d5c
Rafael Fernandes <rafaelsf013@gmail.com>
2023-06-18 14:22:02 -0300

    refactor: Routes

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 import express from 'express'
 import 'dotenv/config'
+import contactRoutes from './routes/contactRoutes.js'
 
 const app = express()
 
 const PORT = process.env.PORT || 8001
 
-app.get('/api/contacts', (req, res) =>{
-    
-    res.status(200).json({message: 'Get all contacts'})
-
-})
+app.use('/api/contacts', contactRoutes)
 
 app.listen(PORT, () => {
 
     console.log(`Server running on port ${PORT}`)
 
 })
-
-
