96b8a9b259ce5f38d11e3c63935a4d5ff5af5f56
Rafael Fernandes <rafaelsf013@gmail.com>
2023-06-19 12:45:52 -0300

    refactor: Route funcions

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,40 @@
+const getContacts = (req, res) =>{
+    
+    res.status(200).json({message: `Get contacts`})
+
+}
+
 const getContact = (req, res) =>{
+    
+    res.status(200).json({message: `Get contact for ${req.params.id}`})
+}
+
+const createContact = (req, res) =>{
+
+    console.log(req.body)
+    
+    res.status(200).json({message: `Create contact`})
+
+}
+
+const updateContact = (req, res) =>{
     
     res.status(200).json({message: `Update contact for ${req.params.id}`})
 
 }
 
-export {getContact}
+const deleteContact = (req, res) =>{
+    
+    res.status(200).json({message: `Delete contact for ${req.params.id}`})
+
+}
+
+export {
+
+    getContacts,
+    getContact,
+    createContact,
+    updateContact,
+    deleteContact,
+
+}
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,31 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import {getContact} from '../controllers/contactController.js'
+import {createContact, deleteContact, getContact, getContacts, updateContact} from '../controllers/contactController.js'
 
-router.get('/', (req, res) =>{
-    
-    res.status(200).json({message: 'Get all contacts'})
-
-})
-
+router.get('/', getContacts)
 router.get('/:id', getContact)
-
-router.post('/', (req, res) =>{
-    
-    res.status(200).json({message: 'Create contacts'})
-
-})
-
-router.put('/:id', (req, res) =>{
-    
-    res.status(200).json({message: `Update contact for ${req.params.id}`})
-
-})
-
-router.delete('/:id', (req, res) =>{
-    
-    res.status(200).json({message: `Delete contact for ${req.params.id}`})
-
-})
+router.post('/', createContact)
+router.put('/:id', updateContact)
+router.delete('/:id', deleteContact)
 
 export default router
