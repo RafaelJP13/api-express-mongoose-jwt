3915a0fc10ca663f000e04fcd67f92af26eb616e
Rafael Fernandes <rafaelsf013@gmail.com>
2023-06-23 10:06:33 -0300

    feat: Add User schema

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const userModel = mongoose.Schema({
+    
+    username: {
+        type: String,
+        required: [true, 'Please add the username!'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add the user email!'],
+        unique:[true, 'Email addres already in use!']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add the user password!'],
+    },
+
+}, {   
+
+    timestamps: true,
+
+})
+
+export default mongoose.model('User', userModel)
