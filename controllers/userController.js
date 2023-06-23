5af4644a0e2a8a1ca0fa7038e0787adbe528622f
Rafael Fernandes <rafaelsf013@gmail.com>
2023-06-23 11:29:07 -0300

    feat: Return user info

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,7 +68,7 @@ const loginUser = asyncHandler(async (req, res) =>{
 
 const currentUser = asyncHandler(async (req, res) =>{
 
-    res.json({message:'Current user information'})
+    res.json(req.user)
 
 })
 
