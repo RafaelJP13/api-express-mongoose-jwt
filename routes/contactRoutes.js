7bd9909f556f1bd1e4223a113b349c6d9cc228da
Rafael Fernandes <rafaelsf013@gmail.com>
2023-06-26 10:01:17 -0300

    feat: Use validateToken middleware for contact routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import {createContact, deleteContact, getContact, getContacts, updateContact} from '../controllers/contactController.js'
+import validateToken from '../middleware/validateTokenHandler.js'
 
 const router = express.Router()
 
+router.use(validateToken)
 router.get('/', getContacts)
 router.get('/:id', getContact)
 router.post('/', createContact)
